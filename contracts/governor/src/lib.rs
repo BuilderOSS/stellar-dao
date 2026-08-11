6844bcd1ea99fc827a902b0eb5a1412808f17b19
dan13ram <dan13ram@gmail.com>
2026-08-11 19:16:08 +0530

    refactor: flatten dao contracts

---
+++ contracts/governor/src/lib.rs
@@ -0,0 +1,8 @@
+#![no_std]
+
+mod governor;
+
+pub use governor::*;
+
+#[cfg(test)]
+mod test;
---
+++ contracts/treasury/src/contract.rs
@@ -0,0 +1,45 @@
+use soroban_sdk::{contract, contractimpl, contracttype, Address, Env, Symbol, Val, Vec};
+
+#[cfg(feature = "mercury")]
+mod retroshade {
+    use super::*;
+    use retroshade_sdk::Retroshade;
+    use soroban_sdk::contracttype;
+
+    #[derive(Retroshade)]
+    #[contracttype]
+    pub struct TreasuryCallIndexed {
+        pub target: Address,
+        pub function: Symbol,
+        pub ledger: u32,
+    }
+}
+
+#[contracttype]
+enum TreasuryKey {
+    Governor,
+}
+
+#[contract]
+pub struct DaoTreasuryContract;
+
+#[contractimpl]
+impl DaoTreasuryContract {
+    pub fn __constructor(e: &Env, governor: Address) {
+        e.storage().instance().set(&TreasuryKey::Governor, &governor);
+    }
+
+    pub fn set_governor(e: &Env, governor: Address) {
+        e.storage().instance().set(&TreasuryKey::Governor, &governor);
+    }
+
+    pub fn governor(e: &Env) -> Address {
+        e.storage().instance().get(&TreasuryKey::Governor).expect("governor not set")
+    }
+
+    pub fn execute(e: &Env, target: Address, function: Symbol, args: Vec<Val>) -> Val {
+        let governor = Self::governor(e);
+        governor.require_auth();
+        e.invoke_contract::<Val>(&target, &function, args)
+    }
+}
